8fad0d57b1f829e7c8ee976b29cb94cd2bc0b25f
classr-22 <roundup414@gmail.com>
2023-07-28 19:11:18 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -16,23 +16,4 @@ app.listen(PORT,async ()=>{
     console.log(`server is running at port: ${PORT}`);
     await connect();
     console.log('mongo db connected');
-    // const tweet = await Tweet.create({
-    //     content: 'third Tweet with hook'
-    // });
-    // const tweet = await Tweet.find({
-    //     userEmail: 'a@b.com'
-    // });
-    // const tweet = await Tweet.findById('64c3677379ce256f7442b0d2');
-    // tweet.userEmail = 'k@c.com';
-    // await tweet.save();
-    // console.log(tweet);
-
-    const tweetrepo = new TweetRepository();
-    const tweet = await tweetrepo.create({content: 'new tweet with hooks',userEmail:'a@$c.com'});
-    // //console.log(tweet);
-    // const comment  = await Comment.create({content: 'second commit',userEmail: 'a@$c.com'});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-    //const tweet = await tweetrepo.getAll(4,2);
-    console.log(tweet);
 })
